58f138e522056d61f3c4f8ba8e9bd2fba84971c9
Chiru <48340051+ch1ru@users.noreply.github.com>
2023-10-13 18:11:37 +0100

    Update init.sql

--- postgresql/init.sql
+++ postgresql/init.sql
@@ -1,1 +1,11 @@
+use DATABASE db;
 
+CREATE TABLE IF NOT EXISTS workspaces
+(
+    id INT NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    data_dir VARCHAR(200) NOT NULL,
+    created DATE NOT NULL,
+    notes VARCHAR(10000) DEFAULT NULL,
+    PRIMARY KEY (id)
+);
