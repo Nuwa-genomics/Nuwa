c8f266d7696eab0cca689635b734a2a8b4a82805
ch1ru <caleb1@criptext.com>
2024-01-29 19:30:11 +0000

    added generate docs file

---
+++ docs/generate_docs.py
@@ -0,0 +1,48 @@
+import ast
+import os
+import glob
+
+path = r'*.py'
+files = []
+root = "../app/pages"
+
+
+python_files = glob.glob("*.py", root_dir=root)
+print(python_files)
+
+
+
+for file in python_files:
+
+        with open(os.path.join(root, file)) as fd:
+            print()
+            print(file)
+            print("Functions:")
+            file_contents = fd.read()
+            module = ast.parse(file_contents)
+                
+            function_definitions = [node for node in module.body if isinstance(node, ast.FunctionDef)]
+            for f in function_definitions:
+                print(f.name)
+                if ast.get_docstring(f):
+                    print(ast.get_docstring(f))
+                else:
+                     print("WARNING: No Doctype")
+                
+
+            class_definitions = [node for node in module.body if isinstance(node, ast.ClassDef)]
+            for class_ in class_definitions:
+                print("\nClass name:", class_.name)
+                methods = [n for n in class_.body if isinstance(n, ast.FunctionDef)]
+                for method in methods:
+                    print(method.name)
+                    if ast.get_docstring(method):
+                        print(ast.get_docstring(method))
+                    else:
+                        print("WARNING: No Doctype")
+
+            print()
+            print()
+            print()
+                    
+
